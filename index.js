814bdc1deb634dcccdc4d9593c5fd1a497f5cd8e
Hamish Macpherson <hamstu@gmail.com>
2020-02-04 22:04:23 -0800

    testing

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ async function run() {
   const myToken = core.getInput('repo-token');
   const octokit = new github.GitHub(myToken);
 
-  console.log('github context => ', github.event);
+  console.log('github event => ', github.event);
+  console.log('github event pr => ', github.event.pull_request);
+  console.log('github context => ', github.context);
+  console.log('github context pr => ', github.context.pull_request);
 
-  const result = await octokit.request('POST /repos/:owner/:repo/issues/:issue_number/comments', {
-    headers: {
-      authorization: `token ${myToken}`,
-    },
-    owner: 'hamstu',
-    repo: 'testing',
-    issue_number: '1',
-    body: core.getInput('comment'),
-  });
+  // const result = await octokit.request(`POST /repos/:owner/:repo/issues/:issue_number/comments`, {
+  //   headers: {
+  //     authorization: `token ${myToken}`,
+  //   },
+  //   // owner: 'hamstu',
+  //   // repo: 'testing',
+  //   // issue_number: github.event.number,
+  //   body: core.getInput('comment'),
+  // });
 
-  console.log(result.data);
+  // console.log(result.data);
 }
 
 run();
