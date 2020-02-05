8fac4ad6298e3d7ed5600a4d8325d84a7dd6337c
Hamish Macpherson <hamstu@gmail.com>
2020-02-04 22:14:18 -0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -192,8 +192,6 @@ async function run() {
   const myToken = core.getInput('repo-token');
   const octokit = new github.GitHub(myToken);
 
-  console.log('github event => ', github.event);
-  console.log('github event pr => ', github.event.pull_request);
   console.log('github context => ', github.context);
   console.log('github context pr => ', github.context.pull_request);
 
