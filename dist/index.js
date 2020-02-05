8d4c08e3b13b04ee56e0c5b0b85cea126ac59b05
Hamish Macpherson <hamstu@gmail.com>
2020-02-04 23:44:25 -0800

    print branch name

--- dist/index.js
+++ dist/index.js
@@ -192,7 +192,13 @@ async function run() {
   const myToken = core.getInput('repo-token');
   const octokit = new github.GitHub(myToken);
 
-  console.log('github context pr => ', github.context.payload.pull_request.comments_url);
+  const branchName = github.context.payload.pull_request.head.ref;
+
+  const body = `
+👋 Hey there! Here are some helpful links related to this PR / branch.
+
+* Your branch name is ${branchName}.
+  `;
 
   const result = await octokit.request(`POST ${github.context.payload.pull_request.comments_url}`, {
     headers: {
@@ -201,7 +207,7 @@ async function run() {
     // owner: 'hamstu',
     // repo: 'testing',
     // issue_number: github.event.number,
-    body: 'Hello on your PR!',
+    body,
   });
 
   console.log(result.data);
--- index.js
+++ index.js
@@ -5,7 +5,13 @@ async function run() {
   const myToken = core.getInput('repo-token');
   const octokit = new github.GitHub(myToken);
 
-  console.log('github context pr => ', github.context.payload.pull_request.comments_url);
+  const branchName = github.context.payload.pull_request.head.ref;
+
+  const body = `
+👋 Hey there! Here are some helpful links related to this PR / branch.
+
+* Your branch name is ${branchName}.
+  `;
 
   const result = await octokit.request(`POST ${github.context.payload.pull_request.comments_url}`, {
     headers: {
@@ -14,7 +20,7 @@ async function run() {
     // owner: 'hamstu',
     // repo: 'testing',
     // issue_number: github.event.number,
-    body: 'Hello on your PR!',
+    body,
   });
 
   console.log(result.data);
