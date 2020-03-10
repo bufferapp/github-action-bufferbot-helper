f457e5ea90d9df7ec83c25d07c5888c81a7a2f47
Hamish Macpherson <hamstu@gmail.com>
2020-03-10 15:29:32 -0700

    v2

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Bufferbot Helper
 
 A simple [GitHub Action](https://help.github.com/en/actions/automating-your-workflow-with-github-actions) for Bufferbot to add helpful context and links to your PRs.
+
+## How to use
+
+Here's an example using this to comment on PRs with helpful links and context:
+
+```yml
+name: Bufferbot Helper
+
+on:
+  pull_request:
+    types: [opened]
+    branches:
+    - master
+
+jobs:
+  pr_comment:
+    runs-on: ubuntu-latest
+    name: PR Comment
+    steps:
+      - uses: bufferapp/github-action-bufferbot-helper@v1
+        with:
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+          action: "pr-comment"
+          staging-url-template: "https://{{placeholder}}-publish.dev.buffer.com"
+          service-name: "buffer-publish"
+```
--- dist/index.js
+++ dist/index.js
@@ -192,16 +192,16 @@ const MAX_SUBDOMAIN_LENGTH = 63;
 const MAX_DEPLOY_MONITOR_LENGTH = 53;
 
 function isAlphaNumeric(str) {
-  const regex = new RegExp('^[a-z0-9]+$', 'i');
+  const regex = new RegExp("^[a-z0-9]+$", "i");
   return regex.test(str);
 }
 
 function getFormattedBranchName(branchName) {
   let formattedBranch = branchName
     .split("")
-    .map(c => (isAlphaNumeric(c) ? c : '-'))
+    .map(c => (isAlphaNumeric(c) ? c : "-"))
     .join("")
-    .toLowerCase();  
+    .toLowerCase();
 
   return formattedBranch;
 }
@@ -209,14 +209,19 @@ function getFormattedBranchName(branchName) {
 // Ref: https://github.com/bufferapp/kuberdash/blob/0f4669411a584846f005686f24680066b089c2c3/src/app/core/k8s/Helm.py#L27
 function getStagingUrl(branchName, stagingUrlTemplate) {
   let formattedBranch = getFormattedBranchName(branchName);
-  
+
   // Ensure the first subdomain is not too long
-  const stagingUrlWithoutPlaceholder = stagingUrlTemplate.replace('{{placeholder}}', '');
-  const lengthOfFirstSubdomain = stagingUrlWithoutPlaceholder.split('.')[0].length;
-  const maxLengthOfFormattedBranch = MAX_SUBDOMAIN_LENGTH - lengthOfFirstSubdomain;
+  const stagingUrlWithoutPlaceholder = stagingUrlTemplate.replace(
+    "{{placeholder}}",
+    ""
+  );
+  const lengthOfFirstSubdomain = stagingUrlWithoutPlaceholder.split(".")[0]
+    .length;
+  const maxLengthOfFormattedBranch =
+    MAX_SUBDOMAIN_LENGTH - lengthOfFirstSubdomain;
   formattedBranch = formattedBranch.substr(0, maxLengthOfFormattedBranch);
 
-  return stagingUrlTemplate.replace('{{placeholder}}', formattedBranch);
+  return stagingUrlTemplate.replace("{{placeholder}}", formattedBranch);
 }
 
 function getDeployMonitorUrl(branchName, serviceName) {
@@ -227,16 +232,53 @@ function getDeployMonitorUrl(branchName, serviceName) {
 }
 
 async function run() {
+  const action = core.getInput("action");
   const myToken = core.getInput("repo-token");
-  const stagingUrlTemplate = core.getInput("staging-url-template");
-  const serviceName = core.getInput("service-name");
+
   const octokit = new github.GitHub(myToken);
 
-  const branchName = github.context.payload.pull_request.head.ref;
-  const stagingUrl = getStagingUrl(branchName, stagingUrlTemplate);
-  const deployMonitorUrl = getDeployMonitorUrl(branchName, serviceName);
+  /**
+   * Simply return the staging URL based on the event
+   * Currently only tested for `status` event on GitHub
+   */
+  if (action === "get-staging-url-when-deployed") {
+    const {
+      context,
+      name,
+      state,
+      description,
+      sha,
+      target_url
+    } = github.context.payload;
+    if (
+      context.startsWith("bufferbotbrains/cicd") &&
+      state === "success" &&
+      description.match(/Build successfully deployed/)
+    ) {
+      // const [owner, repo] = name.split("/");
+      // octokit.checks.create({
+      //   owner,
+      //   repo,
+      //   name: "cypress",
+      //   head_sha: sha,
+      //   status: "in_progress"
+      // });
+      return core.setOutput('stagingUrl', target_url);
+    }
+  }
 
-  const body = `
+  /**
+   * Post a comment on the PR
+   */
+  if (action === "pr-comment") {
+    const stagingUrlTemplate = core.getInput("staging-url-template");
+    const serviceName = core.getInput("service-name");
+    const deployMonitorUrl = getDeployMonitorUrl(branchName, serviceName);
+
+    const branchName = github.context.payload.pull_request.head.ref;
+    const stagingUrl = getStagingUrl(branchName, stagingUrlTemplate);
+
+    const body = `
 👋 Hey there! Here are some helpful links related to this PR / branch.
 
 * Staging URL: ${stagingUrl}
@@ -247,20 +289,19 @@ _Remember, these won't work until the **bufferbotbrains/cicd-buffer-publish** ch
 (*) Requires VPN.
   `;
 
-  const result = await octokit.request(
-    `POST ${github.context.payload.pull_request.comments_url}`,
-    {
-      headers: {
-        authorization: `token ${myToken}`
-      },
-      // owner: 'hamstu',
-      // repo: 'testing',
-      // issue_number: github.event.number,
-      body
-    }
-  );
+    const result = await octokit.request(
+      `POST ${github.context.payload.pull_request.comments_url}`,
+      {
+        headers: {
+          authorization: `token ${myToken}`
+        },
+        body
+      }
+    );
 
-  console.log(result.data);
+    console.log(result.data);
+    return;
+  }
 }
 
 run();
